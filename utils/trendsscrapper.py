5f306aa49a483de3d4e840abd83b576c30001cb9
mi <freitasmillena27@gmail.com>
2024-04-15 17:10:53 +0100

    idk

--- utils/trendsscrapper.py
+++ utils/trendsscrapper.py
@@ -24,7 +24,6 @@ class TrendScraper:
             url,
             data=None,
             headers={
-            #'authority': 'scrapeme.live',
             'dnt': '1',
             'upgrade-insecure-requests': '1',
             'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
