03543adfe11be68098a64e7a4e021cc310390148
mi <freitasmillena27@gmail.com>
2024-04-15 17:09:41 +0100

    added headers to the request

--- utils/trendsscrapper.py
+++ utils/trendsscrapper.py
@@ -24,10 +24,17 @@ class TrendScraper:
             url,
             data=None,
             headers={
-                # Omit unnecessary headers to avoid raising flags
-                "User-Agent": "Mozilla/5.0 (Python; Trends Scraper)",
-                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-            },
+            #'authority': 'scrapeme.live',
+            'dnt': '1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'sec-fetch-site': 'none',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-user': '?1',
+            'sec-fetch-dest': 'document',
+            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+            }
         )
         try:
             with urllib.request.urlopen(req) as f:
