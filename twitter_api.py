b0694f8704b73ae7d2362475a74134cf1928bffe
Guilherme Varela <guilherme.varelaaa@gmail.com>
2024-04-17 12:58:40 +0100

    API Flask done, falta criar os agentes pra comunicarem com a API2

---
+++ twitter_api.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+from twitter.scraper import Scraper
+from datetime import datetime
+
+## sign-in with credentials
+scraper = Scraper("guilherme.lukon@gmail.com", "randommeme65920", "projetoasm123")
+app = Flask(__name__)
+
+@app.route('/tweets/<user>')
+def get_tweets(user):
+    users = scraper.users([user])
+    userid = users[0]["data"]["user"]["result"]["rest_id"]
+    tweets = scraper.tweets([userid], limit=1)
+    
+    entries = None
+    for timeline in tweets[0]["data"]["user"]["result"]["timeline_v2"]["timeline"]["instructions"]:
+        if timeline["type"] == "TimelineAddEntries":
+            entries = timeline
+
+    text = entries["entries"][0]["content"]["itemContent"]["tweet_results"]["result"]["legacy"]["full_text"]
+    date = datetime.strptime(entries["entries"][0]["content"]["itemContent"]["tweet_results"]["result"]["legacy"]["created_at"], '%a %b %d %H:%M:%S %z %Y')
+
+    tweet = {"text": text, "date": date}
+    return jsonify(tweet)
+
+if __name__ == '__main__':
+    app.run(debug=True)
